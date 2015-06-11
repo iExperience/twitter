b86163b8b9f2cfc3e62e5f500705fe98c6b3e9c7
Salman Ansari <salman.ansari@gmail.com>
2015-06-11 16:05:19 +0200

    styled it up

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610140331) do
+ActiveRecord::Schema.define(version: 20150611133125) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string  "body"
-    t.integer "user_id"
-    t.integer "fav_count"
+    t.string   "body"
+    t.integer  "user_id"
+    t.integer  "fav_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "handle"
-    t.string "name"
+    t.string   "handle"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
