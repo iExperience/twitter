30564f2c118ccb7bcfc14d9a50db4891e3828489
Salman Ansari <salman.ansari@gmail.com>
2015-06-12 12:15:40 +0200

    added devise authentication without natural disaster

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets, dependent: :destroy
+
+  validates(:handle, :presence => true)
 end
